019e6ec8a18fee9302f73310550558533ad8755a
Христюк Дарья Алексеевна <@itstep.loc>
2022-11-14 17:40:48 +0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'my-app';
+export class AppComponent implements OnInit{
+  names:string[] = ['dora', 'alina', 'kamilla'] 
+  nameTmp: string = "" 
+  flag:boolean = false 
+  number:number = 0 
+  constructor() { } 
+ 
+  ngOnInit(): void { 
+  } 
+ 
+  addName() { 
+    this.names.push(this.nameTmp) 
+    this.nameTmp="" 
+  } 
+  deleteName(idx:number){ 
+    this.names.splice(idx,1) 
+} 
 }
+
